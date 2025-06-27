988271bad49413afde5eba4a8fcc8dda46c12091
Facundo Colavini <facundo.colavini@cedeira.com.ar>
2025-06-27 12:32:03 -0300

    refactor: update astro.config.mjs and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 // import node from "@astrojs/node";
-import db from "@astrojs/db";
-import vue from "@astrojs/vue";
-import node from "@astrojs/node";
-
 import cloudflare from "@astrojs/cloudflare";
 
+
+import vue from "@astrojs/vue";
+
+import db from "@astrojs/db";
+
+
+
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig( {
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db(), vue()],
-  adapter: cloudflare()
-});
+  integrations: [ mdx(), sitemap(), vue(), db() ],
+  output: "server",
+  adapter: cloudflare(),
+
+  // Desde la versión 4.15, ya no es experimental
+  // experimental: {
+  //   actions: true,
+  // },
+} );
